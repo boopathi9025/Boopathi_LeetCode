07f6d2ace70a0454f4a6085df2c16cef5b8d2c36
Boopathi-tech22 <boopathidrb09@gmail.com>
2025-07-15 17:37:36 +0530

    Time: 1 ms (99.12%), Space: 42.2 MB (47.02%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isValid(String word) {
-        int upper = 0;
-        int lower = 0;
-        int digit = 0;
         int vowel = 0;
         int cons = 0;
         if(word.length() < 3) return false;
@@ -10,17 +7,8 @@ class Solution {
         {
             char ch = word.charAt(i);
             if(!Character.isLetterOrDigit(ch)) return false;
-            if(Character.isUpperCase(ch)) upper++;
-            if(Character.isLowerCase(ch)) lower++;
-            if(Character.isDigit(ch)) digit++;
-            if("aeiouAEIOU".contains(String.valueOf(ch)))
-            {
-                vowel++;   
-            }
-            else if(Character.isLetter(ch))
-            {
-                cons++;
-            }
+            if("aeiouAEIOU".contains(String.valueOf(ch))) vowel++;
+            else if(Character.isLetter(ch)) cons++;
         }
         return vowel > 0 && cons > 0;
     }
